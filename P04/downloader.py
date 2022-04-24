c704f1113a5811dc3d22e0f96c0b410fc2295d54
Diego Fontana <diego.fontana@bluewin.ch>
2022-04-24 19:45:24 +0200

    Added caching to the downloader

--- P04/downloader.py
+++ P04/downloader.py
@@ -1,15 +1,39 @@
+'''
+Downloader for P04, downloads the data for the given link.
+Diego did the Downloader class and MainApplication class
+with one graph. Victor did the rest of the graphs.
+''' 
+__author__ = 'Diego Fontana, Velloso Etter Victor Dominik' 
+__email__ = 'fontadie@students.zhaw.ch, vellovic@students.zhaw.ch'
+
 import requests
+import time, os, stat
   
-# download csv from link
 class Downloader():
+    '''
+    Downloader class
+    '''
+    file_path = 'data.txt'
     def __init__(self, link):
         self.link = link
         self.data = None
 
     def download(self):
-        r = requests.get(self.link)
-        r.encoding = r.apparent_encoding
-        self.data = r.text
+        '''
+        Only downloads the file if the current one is older than 10 minutes
+        '''
+        if os.path.exists(self.file_path) and self.file_age_in_seconds() > 600:
+            r = requests.get(self.link)
+            r.encoding = r.apparent_encoding
+            self.data = r.text
+            with open(self.file_path, 'w') as text_file:
+                text_file.write(self.data)
+        else:
+            with open(self.file_path, 'r') as text_file:
+                self.data = text_file.read()
+
+    def file_age_in_seconds(self):
+        return time.time() - os.stat(self.file_path)[stat.ST_MTIME]
 
 def main():
     link = 'https://www.web.statistik.zh.ch/ogd/data/KANTON_ZUERICH_477.csv'
--- P04/main_application.py
+++ P04/main_application.py
@@ -1,3 +1,11 @@
+'''
+Main application for P04, displays different graphs for the data from the canton Zürich 
+about the building land prices. Diego did the Downloader class and MainApplication class
+with one graph. Victor did the rest of the graphs.
+''' 
+__author__ = 'Diego Fontana, Velloso Etter Victor Dominik' 
+__email__ = 'fontadie@students.zhaw.ch, vellovic@students.zhaw.ch'
+
 from downloader import Downloader
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -9,22 +17,20 @@ class MainApplication():
         d = Downloader(link)
         d.download()
         data = StringIO(d.data)
-        df = pd.read_csv(data, sep=";" )
+        df = pd.read_csv(data, sep=';')
         self.statistical_moments(df)
         self.price_development(df)
         self.expensive(df)
         self.cheap(df)
         self.comparison(df)
-        
     
     def statistical_moments(self,df):
         grouped_by_year = df.groupby(['INDIKATOR_JAHR'])
-        print(grouped_by_year.std()['INDIKATOR_VALUE'])
-        print(grouped_by_year.skew()['INDIKATOR_VALUE'])
-        print(grouped_by_year.apply(pd.DataFrame.kurt)['INDIKATOR_VALUE'])
+        print('Standard deviation:', grouped_by_year['INDIKATOR_VALUE'].std())
+        print('Skewness:', grouped_by_year['INDIKATOR_VALUE'].skew())
+        print('Kurtosis:', grouped_by_year['INDIKATOR_VALUE'].apply(pd.DataFrame.kurt))
         
-        
-    def price_development(self,df):    
+    def price_development(self,df): 
         years = df['INDIKATOR_JAHR'].unique()
         plt.title('Baulandpreisentwicklung im Kanton Zürich')
         plt.plot(years, df.groupby(['INDIKATOR_JAHR']).mean()['INDIKATOR_VALUE'])
@@ -36,13 +42,12 @@ class MainApplication():
         plt.show()
         
     def expensive(self,df): 
-        #Bar plot expensive locations in ZH
+        '''
+        Bar plot expensive locations in ZH
+        '''
         new_df = df.groupby('GEBIET_NAME').mean('INDIKATOR_VALUE').reset_index()
         new_df = new_df.sort_values('INDIKATOR_VALUE')
         new_df = new_df.tail(5)
-        print(new_df)
-        
-        
         plt.bar(new_df['GEBIET_NAME'],new_df['INDIKATOR_VALUE'])
         plt.ylim(1100,1400)
         plt.ylabel('Preise pro m2')
@@ -50,27 +55,27 @@ class MainApplication():
         plt.show()
         
     def cheap(self,df):
-        # Bar Plot cheap locations in ZH
+        '''
+        Bar Plot cheap locations in ZH
+        '''
         cheap_df = df.groupby('GEBIET_NAME').mean('INDIKATOR_VALUE').reset_index()
         cheap_df = cheap_df.sort_values('INDIKATOR_VALUE')
         cheap_df = cheap_df.head(5)
-        
-        
         plt.bar(cheap_df['GEBIET_NAME'],cheap_df['INDIKATOR_VALUE'])
         plt.title('Günstigste Ortschaften im Kanton Zürich')
-        plt.ylabel("Preise pro m2")
+        plt.ylabel('Preise pro m2')
         plt.ylim(140,250)
         plt.show()
         
-        
     def comparison(self,df):
-        #filter cheapest and most expensive locations to watch the price development
+        '''
+        filter cheapest and most expensive locations to watch the price development
+        '''
         compare_df = df.loc[df['GEBIET_NAME'] == 'Fischenthal']
         plt.plot(compare_df['INDIKATOR_JAHR'],compare_df['INDIKATOR_VALUE'], label = 'Fischenthal')
         plt.title('Vergleich teuerste vs günstigste Ortschaft')
         plt.ylabel('Preis pro m2')
-        
-        compare_df = df.loc[df['GEBIET_NAME'] == 'ZÃ¼rich']
+        compare_df = df.loc[df['GEBIET_NAME'] == 'Zürich']
         plt.plot(compare_df['INDIKATOR_JAHR'],compare_df['INDIKATOR_VALUE'], label = 'Zürich')
         plt.legend()
         plt.show()
@@ -80,6 +85,5 @@ def main():
     main_application = MainApplication()
     main_application.main()
     
-
 if __name__ == '__main__':
     main()
