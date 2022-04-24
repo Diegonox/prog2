6cad2b1cf8352cd0325724d74e3e25e206d70c30
vic <victor_etter@hotmail.com>
2022-04-24 15:17:05 +0200

    Fixed UTF8 encoding, made methods out of spaghetti code

--- P04/downloader.py
+++ P04/downloader.py
@@ -8,12 +8,13 @@ class Downloader():
 
     def download(self):
         r = requests.get(self.link)
+        r.encoding = r.apparent_encoding
         self.data = r.text
 
 def main():
     link = 'https://www.web.statistik.zh.ch/ogd/data/KANTON_ZUERICH_477.csv'
     d = Downloader(link)
     d.download()
-
+    
 if __name__ == '__main__':
     main()
