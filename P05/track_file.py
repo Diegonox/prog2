a6af9851b2ff951a53b8d66e41ca3e247d0e6584
Oscar Echeverry <echevosc@students.zhaw.ch>
2022-05-29 18:43:28 +0200

    trac_file as class

--- P05/track_file.py
+++ P05/track_file.py
@@ -1,20 +1,13 @@
-import requests
 import json
+from transport_api_service import TransportApiService
+
 
 """
 Call transport.opendata.ch api to get the connections between two locations.
 """
 
-
-
-class TransportApiService:
+class TrackFile:
     def __init__(self):
-        self.base_api_url = 'https://transport.opendata.ch/v1/'
-        self.connections_api_url = self.base_api_url + 'connections'
-        self.connections_api_params = {
-            'from': '',
-            'to': '',
-        }
         self.cities = ['Salzburg','Graz',
             'Bregenz', 'Feldkirch','Dornbirn','Vienne',
             'Frankfurt am Main','Köln','Wallhausen',
@@ -29,20 +22,6 @@ class TransportApiService:
             'Cadaqués', 'Málaga','Murcia','Valencia',
             'Madrid','Saragosa','Barcelona','Bilbao','Valladolid',
             'Milano','Bologna', 'Aosta', 'Domodossola','como', 'Varese']
-    
-    def get_connections(self, from_location, to_location):
-        self.connections_api_params['from'] = from_location
-        self.connections_api_params['to'] = to_location
-        try:
-            r = requests.get(self.connections_api_url,
-                             params=self.connections_api_params)
-            r.raise_for_status()
-            return r.json()
-        except requests.exceptions.HTTPError as err:
-            print(err)
-        except requests.exceptions.RequestException as e:
-            print(e)
-            return e
         
     def create_track_file(self, city_from):
         '''
@@ -65,12 +44,19 @@ class TransportApiService:
                 long = result2['to']['station']['coordinate']['y']
                 prov_file[city] = {'access': 'reachable','x':lat,'y':long, 'station_to':s_to}
         track_file[city_from] = prov_file
-        dic_string = json.dumps(track_file, indent=2)
+        dic_string = json.dumps(track_file, indent=4)
 
-        with open ('track_dict', 'w') as f:
+        with open ('track_dict.json', 'w') as f:
             f.write(dic_string)
             
-            
+
+    def update_track_file(self, from_city):
+
+        ''' creates a new dict with connections information and
+        adds it to the existing track_file
+        '''
+# to be completed
+
             
     def count_reachables(self, track_dict:str, home_location):
         with open(track_dict, 'r') as f:
@@ -84,11 +70,14 @@ class TransportApiService:
                         count_reach += 1
                 else:
                     count_unreach += 1
-        print ('reachable = ', count_reach, '\n', 'unreachable = ', count_unreach)
+        return (count_reach, count_unreach)
 
 
 if __name__ == '__main__':
     
 
-    t = TransportApiService()
-    file = t.create_track_file('Zürich')
+    t = TrackFile()
+    t.create_track_file('Zürich')
+    #reach_from_zürich = t.count_reachables('track_dict.json', 'Zürich')
+    #print(reach_from_zürich)
+
