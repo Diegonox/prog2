860eff572acd125ebb7b45bbc262519702479b41
Oscar Echeverry <echevosc@students.zhaw.ch>
2022-05-30 17:41:27 +0200

    + method check_city

--- P05/main/blacklist.py
+++ P05/main/blacklist.py
@@ -1,31 +1,46 @@
+
+
 import csv
+from genericpath import exists
+
+# the language of the blacklist should be defined or saved in different languages? 
 
 class BlackList:
 
     def __init__(self):
         pass
 
-    def create_blacklist(self):
-        '''this function can also be used to erease/reset de blacklist. 
-        '''
+    def create_blacklist(self): #this function can also be used to reset de blacklist
 
         with open ('blacklist.csv', 'w') as blacklist:
-            fieldnames = ['city', 'x', 'y'] # the coordinates should help if repeated names for different cities
+            fieldnames = ['city', 'country'] 
             writer = csv.DictWriter(blacklist, fieldnames=fieldnames)
             writer.writeheader()
         
 
-
-    def insert_city(self, city, x, y): # x and y could be also arguments, if they will be used for other purposes
+    def insert_city(self, city, country):
 
         with open('blacklist.csv', "a", newline="") as blacklist:
             writer = csv.writer(blacklist)
-            writer.writerow([city, x, y])
+            writer.writerow([city, country])
+
+    def check_city(self, city): # checks if the city exists in the blacklist. 
+        
+        is_in_list = False
+        cities = []
+        with open('blacklist.csv', 'r') as blacklist:
+            rows = csv.reader(blacklist)
+            for row in rows:
+                if city in str(row):
+                    is_in_list = True
+        return is_in_list
+
 
 if __name__ == '__main__':
     f = BlackList()
     f.create_blacklist()
-    f.insert_city('Berlin', '9.99', '10.5')
+    f.insert_city('Berlin', 'Deutschland')
+    print(f.check_city('Berlin'))
 
 
 
