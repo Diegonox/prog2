1c95a51f2077ff6ee11877e09d7ad039a2246ae6
vic <victor_etter@hotmail.com>
2022-05-31 23:04:39 +0200

    Added reccomendation of search engines

--- P05/services/percentage_traveled.py
+++ P05/services/percentage_traveled.py
@@ -19,6 +19,7 @@ class DistanceCalculator:
     def calculate_closest_location(self, reachable_stations, destination_station, start_station):
         closest_distance = 200000000
         closest_station = None
+        link = self.alternate_link(destination_station)
         destination_coord = (destination_station.long, destination_station.lat)
         total_distance = (geopy.distance.geodesic((start_station.long, start_station.lat), destination_coord).km)
         for station in reachable_stations:
@@ -30,18 +31,21 @@ class DistanceCalculator:
                 closest_distance = temp_distance
                 closest_station = station
 
-        return self.calculate_percent_and_time(total_distance,closest_station, start_station)
+        return self.calculate_percent_and_time(total_distance,closest_station, start_station,link)
 
-    def calculate_percent_and_time(self,total_distance ,closest_station, start_station):
+    def calculate_percent_and_time(self,total_distance ,closest_station, start_station,link):
         coords_1 = (start_station.long,start_station.lat)
         coords_2 = (closest_station.long,closest_station.lat)
         reachable_distance = (geopy.distance.geodesic(coords_1,coords_2).km)
         # calculate how much of the way has been reached through our app
         percent = round((reachable_distance/total_distance) * 100, 2)
         duration = self.travel_time(start_station.city,closest_station.city)
-        return f'Closest Location to your final Destination:{closest_station.city}\n' \
+        return f'Closest Location to your final Destination: {closest_station.city}\n' \
                f'{percent}% of the way travelled\n' \
-               f'Duration:  {duration}'
+               f'Duration: {duration}\n' \
+               f'More Info at: {link}'
+
+
 
 
     def travel_time(self, start_station, closest_station):
@@ -49,4 +53,20 @@ class DistanceCalculator:
         r = requests.get(link)
         loc = r.json()
         return loc['connections'][0]['duration']
+
+    def alternate_link(self,destination_station):
+        searchlinks = {'Niederlände': 'https://www.connexxion.nl/de/fahrkarten',
+                       'Spanien': 'https://www.renfe.com/es/es',
+                       'Deutschland': 'https://www.bahn.de/',
+                       'Österreich': 'https://www.oebb.at/',
+                       'Italien': 'https://www.trenitalia.com/de.html',
+                       'Belgien': 'https://www.belgiantrain.be/de'}
+        try:
+            link = searchlinks[destination_station.country]
+            return link
+        except KeyError:
+            link = ''
+            return link
+
+
 
