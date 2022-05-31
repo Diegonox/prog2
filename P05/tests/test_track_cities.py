2e9c7d29e0210ea3f904ebd46c43257c574b978a
Oscar Echeverry <echevosc@students.zhaw.ch>
2022-05-31 18:48:32 +0200

    test track_cities

---
+++ P05/tests/test_track_cities.py
@@ -0,0 +1,13 @@
+import unittest
+from ..main.track_cities import TrackCities
+
+
+
+class TestTrackCities(unittest.TestCase):
+
+    def test_track(self):
+        #to do
+        
+
+if __name__ == '__main__':
+    unittest.main()
