e22f8c8b9f2ab7437720cdac74234641ed2956d4
Oscar Echeverry <echevosc@students.zhaw.ch>
2022-05-31 22:06:51 +0200

    part of test_trac_cities

--- P05/tests/test_track_cities.py
+++ P05/tests/test_track_cities.py
@@ -2,11 +2,30 @@ import unittest
 from ..main.track_cities import TrackCities
 
 
-
 class TestTrackCities(unittest.TestCase):
 
-    def test_track(self):
-        #to do
+    def test_track_cities(self):
+
+        self.cities = ['Salzburg', 'Graz',
+                       'Bregenz', 'Feldkirch', 'Dornbirn', 'Vienne',
+                       'Frankfurt am Main', 'Köln', 'Wallhausen',
+                       'Ulm', 'Stuttgart Hbf', 'Freiburg im Breisgau', 'Bodman-Ludwigshafen',
+                       'Hamburg', 'Konstanz', 'Ravensburg', 'Karlsruhe',
+                       'Arnhem', 'Amsterdam', 'Venlo', 'Den Haag',
+                       'Paris', 'Lyon', 'Marseille', 'Mühlhausen',
+                       'Besançon', 'Grenoble', 'Nice', 'Charleroi',
+                       'London', 'Manchester', 'Liverpool', 'Birmingham', 'Leeds',
+                       'Prag',
+                       'Dublin', 'Belfast', 'Glasgow',
+                       'Cadaqués', 'Málaga', 'Murcia', 'Valencia',
+                       'Madrid', 'Saragosa', 'Barcelona', 'Bilbao', 'Valladolid',
+                       'Milano', 'Bologna', 'Aosta', 'Domodossola', 'como', 'Varese']
+
+        self.city_from = 'Zurich'
+
+
+        self.assertEqual()
+        self.assertEqual()
         
 
 if __name__ == '__main__':
