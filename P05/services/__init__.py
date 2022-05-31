3d884a1a3239180fc0245669f01c4b73b1bf0d3f
Diego <diego.fontana@bluewin.ch>
2022-05-31 20:10:24 +0200

    init change

--- P05/services/__init__.py
+++ P05/services/__init__.py
@@ -1,3 +1,4 @@
 from .subset_calculator import SubsetCalculator
 from .transport_api_service import TransportApiService
 from .transport_service import TransportService
+from .response_types import *
